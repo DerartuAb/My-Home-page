a11b97c6af60e9a323dcb9b6b0dd0645bb10d5bd
DerartuAb <dabdisa@miu.edu>
2022-07-26 00:46:28 -0500

    bank

---
+++ Bank/index.js
@@ -0,0 +1,42 @@
+
+window.onload = function () {
+    console.log(window.localStorage);
+    if (window.localStorage.accounts) {
+        let textarea = document.getElementById("textarea");
+        textarea.innerHTML = window.localStorage.accounts;
+		localStorage.clear();
+    }
+}
+
+class Account {
+
+    static accountInfoList = [];
+
+    constructor(name, amount) {
+		
+        this.name = name;
+        this.balance = amount;
+        Account.accountInfoList.push(this);
+
+        // console.log("account is created" + " " + Account.accountInfoList.length);
+    }
+
+    static accountInfo() {
+        let accounts = " ";
+
+        for (let i = 0; i < Account.accountInfoList.length; i++) {
+            accounts += "Account name: " + Account.accountInfoList[i]['name'] + ", "
+                + "Balance: " + ", " + Account.accountInfoList[i].balance + "\n";
+        }
+        return accounts;
+
+    }
+
+    deposite(amount) {
+        this.balance += amount;
+    }
+    withdraw(amount) {
+        this.balance -= amount;
+    }
+
+}
---
+++ Bank/index1.js
@@ -0,0 +1,33 @@
+
+let createAccountBtn = document.getElementById("create-account-btn");
+
+createAccountBtn.addEventListener('click', createAccount);
+
+function createAccount() {
+
+    let accName = document.getElementById("drop-down").value;
+    let amount = document.getElementById("amount").value;
+
+
+    new Account(accName, amount);
+	
+    if (window.localStorage.accounts) {
+        let a = window.localStorage.accounts;
+        let b = Account.accountInfo();
+        a += b;
+        window.localStorage.accounts = a;
+    }
+    else {
+        window.localStorage.accounts = Account.accountInfo()
+
+    }
+
+}
+
+
+
+
+
+
+
+
